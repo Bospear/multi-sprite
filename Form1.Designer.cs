394bc2930186fb20a05eb11939b3b4b4598594a2
aleksandarrajkovicbalto <aleksandarrajkovic90@gmail.com>
2023-09-29 22:27:56 +0200

    MVP

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_export = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_export
+            // 
+            this.btn_export.Location = new System.Drawing.Point(1030, 483);
+            this.btn_export.Name = "btn_export";
+            this.btn_export.Size = new System.Drawing.Size(75, 23);
+            this.btn_export.TabIndex = 0;
+            this.btn_export.Text = "Export";
+            this.btn_export.UseVisualStyleBackColor = true;
+            this.btn_export.Click += new System.EventHandler(this.btn_export_Click);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(1134, 518);
+            this.Controls.Add(this.btn_export);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragEnter);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_export;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,18 +3,190 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace multi_sprite
 {
     public partial class Form1 : Form
     {
+        string path;
+        bool file_valid;
+        Thread TGetImage;
+        Image image;
+        List<PictureBox> SpriteSheets = new List<PictureBox>();
+        int freeX = 0, freeY = 0;
+        int deletion_index = 0;
+        bool key_down = false;
+        bool sprite_sheet_selected = false;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        bool GetFileName(out string filename, DragEventArgs e) 
+        {
+            filename = String.Empty;
+            bool file_proper = false;
+
+            if((e.AllowedEffect & DragDropEffects.Copy) == DragDropEffects.Copy)
+            {
+                Array file_data = ((IDataObject)e.Data).GetData("FileDrop") as Array;
+                if(file_data == null)
+                    return false;
+                if((file_data.Length > 0) && (file_data.GetValue(0) is String))
+                {
+                    filename = ((string[])file_data)[0];
+                    string extention = Path.GetExtension(filename).ToLower();
+                    if(extention == ".png")
+                    {
+                        file_proper = true;
+                    }
+                }
+            }
+            return file_proper;
+        }
+        protected void LoadImage()
+        {
+            image = new Bitmap(path);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            string filename;
+            file_valid = GetFileName(out filename, e);
+            if (file_valid)
+            {
+                path = filename;
+                TGetImage = new Thread(new ThreadStart(LoadImage));
+                TGetImage.Start();
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            if (file_valid)
+            {
+                while (TGetImage.IsAlive)
+                {
+                    Application.DoEvents();
+                    Thread.Sleep(0);
+                }
+                PictureBox tempPictureBox = new PictureBox();
+                tempPictureBox.Image = image;
+                tempPictureBox.Location = new Point(freeX, freeY);
+                tempPictureBox.Width = image.Width;
+                tempPictureBox.Height = image.Height;
+                tempPictureBox.Name = SpriteSheets.Count.ToString();
+                tempPictureBox.Click += spriteSheet_OnClick;
+                freeX += image.Width;
+
+                SpriteSheets.Add(tempPictureBox);
+
+                this.Controls.Add(tempPictureBox);
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (key_down)
+                return;
+            if (!sprite_sheet_selected)
+                return;
+            if (SpriteSheets.Count < 1)
+                return;
+            if(e.KeyCode == Keys.Delete)
+            {
+                sprite_sheet_selected = false;
+                key_down = true;
+                SpriteSheets.RemoveAt(deletion_index);
+                this.Controls.RemoveByKey(deletion_index.ToString());
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete)
+            {
+                key_down = false;
+            }
+        }
+
+        private void btn_export_Click(object sender, EventArgs e)
+        {
+            if (SpriteSheets.Count < 1)
+                return;
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.InitialDirectory = @"E:\";      
+            saveFileDialog1.Title = "Save Sprite Sheet";
+            saveFileDialog1.CheckFileExists = false;
+            saveFileDialog1.CheckPathExists = true;
+            saveFileDialog1.DefaultExt = "png";
+            saveFileDialog1.Filter = "Image (*.png)|*.png";
+            saveFileDialog1.FilterIndex = 2;
+            saveFileDialog1.RestoreDirectory = true;
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                Stream myStream;
+                if ((myStream = saveFileDialog1.OpenFile()) != null)
+                {
+                    Image exportImage = new Bitmap(SpriteSheets[0].Image);
+                    for(int i = 1; i < SpriteSheets.Count; i++)
+                    {
+                        exportImage = MergedBitmaps(exportImage, SpriteSheets[i].Image as Bitmap, SpriteSheets[i].Location);
+                    }
+                    
+                    exportImage.Save(myStream, System.Drawing.Imaging.ImageFormat.Png);
+                    myStream.Close();
+                }
+            }
+        }
+        private Image MergedBitmaps(Image bmp1, Image bmp2, Point bmp2_location)
+        {
+            Image result = new Bitmap(bmp1.Width + bmp2.Width, bmp1.Height);
+            using (Graphics g = Graphics.FromImage(result))
+            {
+                g.DrawImage(bmp1, new Point(0,0));
+                g.DrawImage(bmp2, bmp2_location);
+               
+            }
+            return result;
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            SpriteSheets[deletion_index].BackColor = Color.Gray;
+            deletion_index = 0;
+            sprite_sheet_selected = false;
+        }
+
+        private void spriteSheet_OnClick(object sender, EventArgs e)
+        {
+            PictureBox tempPictureBox = sender as PictureBox;
+            if (tempPictureBox == null)
+                return;
+            int index = Int32.Parse(tempPictureBox.Name);
+            if(sprite_sheet_selected)
+                SpriteSheets[deletion_index].BackColor = Color.Gray;
+
+            deletion_index = index;
+            SpriteSheets[index].BackColor = Color.Aquamarine;
+            sprite_sheet_selected = true;
         }
     }
 }
